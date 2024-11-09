667dc19960dd47342e56c96650d527ae31334b0e
ItsMeMahiG <mahi.gadicherlaboba@gmail.com>
2024-11-09 19:34:00 +0530

    ahhhh my bday!!

---
+++ weather.py
@@ -0,0 +1,6 @@
+def weather() :
+    print("weather is plesent in",rainyseason)
+    print("weather is cold in", winter)
+winter= "winter"
+rainyseason="monsoon"
+weather()
---
+++ well wishes.py
@@ -0,0 +1,6 @@
+def wellwishes() :
+    print("hello!")
+    print("MY BDAY IS IN 5 DAYSS!!")
+    print("GIVE ME GIFTS AND GET CAKEE!")
+
+wellwishes()
